0180d1b1b77586cee3012d3d116eafae782fe521
rsingh9430 <rsingh9430@gmail.com>
2022-12-15 18:37:06 +0530

    added active subdomain recon

--- subdomain.sh
+++ subdomain.sh
@@ -3,11 +3,21 @@
 dir=~/Recon/$1/$2;
 mkdir -p $dir
 
+echo "[+] Starting passive subdomain recon..."
 subfinder -d $2 > $dir/$2_unfilter_subdomains;
 assetfinder --subs-only $2 >> $dir/$2_unfilter_subdomains;
+echo"[+] wait for amass passive recon"
+amass enum -passive -d $2 -o $dir/$2_unfilter_subdomains;
 
-amass enum -d $2 >> $dir/$2_unfilter_subdomains;
+echo "[+} Starting active subdomain recon...."
+amass enum -active -d $2 -brute -w /opt/wordlists/assetnotes/data/manual/2m-subdomains.txt -o $dir/$2_unfilter_subdomains;
+
+echo "[+] Shorting unqiue subdomains...."
 cat $dir/$2_unfilter_subdomains | sort -u > $dir/$2_subdomains;
+
+echo "[+] Checking for alive domains..."
+cat $dir/$2_subdomains | httprobe --prefer-https -c 50 >> $dir/$2_alive ;
+
 
 #learn how to take parameter in bash
 #learn how to take parmater with value like -u = google.com``
--- take_screenshot.sh
+++ take_screenshot.sh
@@ -2,5 +2,5 @@
 
 dir=~/Recon/$1/$2
 file=$dir'/'$2'_alive'
-python3 ~/Tools/EyeWitness/Python/EyeWitness.py --web -f $file -d $dir/screenshot/ --no-prompt  --timeout 15
+python3 ~/Tools/EyeWitness/Python/EyeWitness.py --web -f $file -d $dir/screenshot/ --no-prompt  --timeout 30
 echo $file
